04ba90caff74cc30312aa9d1b908be9eda743799
Adesh Srivastava <adeshusa@gmail.com>
2026-02-21 18:07:37 -0500

    supposed fix for front end to engine issues

--- api.py
+++ api.py
@@ -29,7 +29,7 @@ from utils.gemini_chat import chat_with_stats
 app = FastAPI(title="Big-Dawg API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
